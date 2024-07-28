6f7810ad991c512dd580b02d07046e6250d81752
davidting <david.ting@tksring.com>
2024-07-29 00:34:55 +0800

    Add 20240729 daily challenge template

---
+++ 2045-second-minimum-time-to-reach-destination.py
@@ -0,0 +1,4 @@
+# https://leetcode.com/problems/second-minimum-time-to-reach-destination/description/
+class Solution:
+    def secondMinimum(self, n: int, edges: list[list[int]], time: int, change: int) -> int:
+        pass
