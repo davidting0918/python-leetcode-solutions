3524f6fbad89250a4dbf9d69befe9d22a204c859
davidting <david.ting@tksring.com>
2024-08-02 12:03:03 +0800

    Add 2134 finished code

--- 2134-minimum-swaps-to-group-all-1s-together-ii.py
+++ 2134-minimum-swaps-to-group-all-1s-together-ii.py
@@ -17,28 +17,24 @@ class Solution:
 
         return min_swap
 
-
-    def ninSwaps_v2(self, nums: list[int]) -> int:
+    def minSwaps_v2(self, nums: list[int]) -> int:
         total_one = sum(nums)
         n = len(nums)
-        min_swap = float("inf")
+        max_one_in_window = current_one = sum(nums[:total_one])
 
-        circular_nums = nums + nums[:total_one]
-
-        # sliding window
-        for start in range(2*n - total_one + 1):
-            subarray = circular_nums[start:start + total_one]
-            min_swap = min(min_swap, total_one - sum(subarray))
-            # print(f"Start: {start}, end: {start + total_one}, Subarray: {subarray}, Min swap: {min_swap}")
-
-        return min_swap
+        print(f"Range: {total_one} to {n + total_one}")
+        for i in range(total_one, n + total_one):  # need to loop to n + total_one to cover circular array
+            current_one += nums[i % n]
+            current_one -= nums[(i - total_one ) % n]
+            print(f"Add index: {i % n}, Remove index: {(i - total_one + n) % n}, Current one: {current_one}")
+            max_one_in_window = max(max_one_in_window, current_one)
+        return total_one - max_one_in_window
 
 
 if __name__ == '__main__':
     s = Solution()
     nums = [1,0,1,0,1]
-    print(s.minSwaps(nums))
-
+    print(s.minSwaps_v2(nums))
 
     nums = [1, 0, 0, 0, 0, 1, 0, 1]
-    print(s.minSwaps(nums))
+    print(s.minSwaps_v2(nums))
