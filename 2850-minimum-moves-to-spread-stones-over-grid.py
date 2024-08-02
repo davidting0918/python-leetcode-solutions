fa5becbca5a045f3dae43178eaeb00f11048519d
davidting0918 <dting0918@gmail.com>
2024-08-02 18:52:22 +0800

    Add 2850 finished code

--- 2850-minimum-moves-to-spread-stones-over-grid.py
+++ 2850-minimum-moves-to-spread-stones-over-grid.py
@@ -11,11 +11,36 @@ class Solution:
                 if grid[i][j] == 0:
                     target.append((i, j))
                 elif grid[i][j] != 1:
-                    source.append((i, j) * (grid[i][j] - 1))
+                    for _ in range(grid[i][j] - 1):
+                        source.append((i, j))
 
         # list all combinations of source and target, total combination is len(source)! and use f"{source} to {target}" as key
+        results = []
+        self._recursive_run(len(source), [False] * len(source), [], results, source, target)
+        return min(results)
+    
+    def _recursive_run(self, n, used, current, results, s, t):
+        if len(current) == n:
+             results.append(
+                 sum(
+                    abs(s[i][0] - t[j][0]) + abs(s[i][1] - t[j][1])
+                    for i, j in enumerate(current)
+                 )
+             )
+        
+        for i in range(n):
+            if not used[i]:
+                used[i] = True
+                current.append(i)
+                self._recursive_run(n, used, current, results, s, t)
+                current.pop()
+                used[i] = False
+        
 
 if __name__ == '__main__':
     s = Solution()
     grid = [[1,1,0],[1,1,1],[1,2,1]]
+    print(s.minimumMoves(grid))
+    
+    grid = [[1,3,0],[1,0,0],[1,0,3]]
     print(s.minimumMoves(grid))
