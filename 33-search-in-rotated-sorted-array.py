b9b9484fb46369d9916578b7cdab2da13586ad5d
David Ding <davidding@kronosresearch.com>
2024-08-06 10:32:35 +0000

    Add 33 template

---
+++ 33-search-in-rotated-sorted-array.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        return 
+    
+
+if __name__ == "__main__":
+    s = Solution()
+
+    nums = [4,5,6,7,0,1,2]
+    target = 0
+    print(nums)
+    print(target)
+    print(s.search(nums, target))
