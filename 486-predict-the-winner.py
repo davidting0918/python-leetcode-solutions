387d1e2b9873ecb76cf54995ff2c371bc23354cd
davidting <david.ting@tksring.com>
2024-08-09 17:46:29 +0800

    Add 486 format

---
+++ 486-predict-the-winner.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/predict-the-winner/description/
+from typing import List
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        return
+
+
+if __name__ == "__main__":
+    s = Solution()
+    nums = [1, 5, 2]
+    print(s.predictTheWinner(nums))
