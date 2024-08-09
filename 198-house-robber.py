bf0a7c296910b7f49465bbb4003a51da7e43a253
davidting <david.ting@tksring.com>
2024-08-09 17:42:09 +0800

    Add 198 format

---
+++ 198-house-robber.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/house-robber/description/
+from typing import List
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        return
+
+
+if __name__ == "__main__":
+    s = Solution()
+    nums = [1,2,3,1]
+    print(s.rob(nums))
