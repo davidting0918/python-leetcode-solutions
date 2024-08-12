5b8eccbaac3fb7067c1a2a04b16c78182d989448
David Ding <davidding@kronosresearch.com>
2024-08-12 09:22:59 +0000

    Add 121 finished code

--- 121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock.py
@@ -3,11 +3,16 @@ from typing import List
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         min_price = float('inf')
-        dp = [0] * len(prices)
-        for i in range(1, len(prices)):
-            min_price = min(min_price, prices[i-1])
-            dp[i] = max(dp[i-1], prices[i] - min_price)
-        return dp[-1]
+        max_profit = 0
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            profit = price - min_price
+
+            if profit > max_profit:
+                max_profit = profit
+        return max_profit
 
 if __name__ == "__main__":
     s = Solution()
