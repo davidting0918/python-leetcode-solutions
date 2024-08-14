9ffc502b89f2fb6a2f079a729e0c0d309836d0f7
davidting <david.ting@tksring.com>
2024-08-14 23:30:00 +0800

    Add 719 finished code

--- 719-find-k-th-smallest-pair-distance.py
+++ 719-find-k-th-smallest-pair-distance.py
@@ -3,8 +3,21 @@ from typing import List
 class Solution:
     def smallestDistancePair(self, nums: List[int], k: int) -> int:
         nums.sort()
-        low, high = nums[1] - nums[0], nums[-1] - nums[0]
-    
+        low, high = 0, nums[-1] - nums[0]
+
+        while low < high:
+            mid = low + (high - low) // 2
+            count, left = 0, 0
+            for right in range(len(nums)):
+                while nums[right] - nums[left] > mid:
+                    left += 1
+                count += right - left
+            if count < k:
+                low = mid + 1
+            else:
+                high = mid
+
+        return low
 
 if __name__ == "__main__":
     s = Solution()
