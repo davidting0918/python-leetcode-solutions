8575106446fab00ca9a491a6bc829188075f6ba8
davidting <david.ting@tksring.com>
2024-08-17 00:51:19 +0800

    Add 221 template

---
+++ 221-maximal-square.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/maximal-square/description/?envType=study-plan-v2&envId=dynamic-programming
+from typing import List
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        return
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.maximalSquare([["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]))
