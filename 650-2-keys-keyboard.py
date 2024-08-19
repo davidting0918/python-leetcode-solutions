f4496cbbe9c0afc81cadbdd4782d2a9eb544afd4
davidting <david.ting@tksring.com>
2024-08-19 21:26:00 +0800

    Add 650 finished code;

--- 650-2-keys-keyboard.py
+++ 650-2-keys-keyboard.py
@@ -1,9 +1,20 @@
 # https://leetcode.com/problems/2-keys-keyboard/description/?envType=daily-question&envId=2024-08-19
 class Solution:
     def minSteps(self, n: int) -> int:
-        return
-    
+        result = 0
+
+        start = 2
+        while n > 1:
+            num, remainder = divmod(n, start)
+            if remainder == 0:
+                result += start
+                n = num
+                start = 2
+                continue
+            start += 1
+            
+        return result
 
 if __name__ == "__main__":
     sol = Solution()
-    print(sol.minSteps(3))
+    print(sol.minSteps(4))
