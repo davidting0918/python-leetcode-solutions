8bc8b239e680ced35f035e20c266a6ec649b542c
David Ding <davidding@kronosresearch.com>
2024-08-19 10:44:31 +0000

    Add 650 template

---
+++ 650-2-keys-keyboard.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/2-keys-keyboard/description/?envType=daily-question&envId=2024-08-19
+class Solution:
+    def minSteps(self, n: int) -> int:
+        return
+    
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.minSteps(3))
