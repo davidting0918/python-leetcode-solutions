864f918043c4613c817e7bc3af4dc8ef1b2cd7b8
davidting <david.ting@tksring.com>
2024-09-09 23:47:31 +0800

    Add 2326 finished code

--- 2326-spiral-matrix-iv.py
+++ 2326-spiral-matrix-iv.py
@@ -8,4 +8,26 @@ class ListNode:
         self.next = next
 class Solution:
     def spiralMatrix(self, m: int, n: int, head: Optional[ListNode]) -> List[List[int]]:
-        return
+        outputs = [
+            [-1] * n for _ in range(m)
+        ]
+
+        x, y = 0, 0
+        index = 0
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+
+        while head:
+            outputs[x][y] = head.val
+            head = head.next
+
+            dx, dy = directions[index]
+            nx, ny = x + dx, y + dy
+
+            if nx < 0 or nx >= m or ny < 0 or ny >= n or outputs[nx][ny] != -1:
+                index = (index + 1) % 4
+                dx, dy = directions[index]
+                nx, ny = x + dx, y + dy
+
+            x, y = nx, ny
+
+        return outputs
