3e6f4e33096001797547d16026ca7310a4522904
David Ding <davidding@kronosresearch.com>
2024-09-11 07:28:38 +0000

    Add 2220 strucutre

---
+++ 2220-minimum-bit-flips-to-convert-number.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/minimum-bit-flips-to-convert-number/description/?envType=daily-question&envId=2024-09-11
+class Solution:
+    def minBitFlips(self, start: int, goal: int) -> int:
+        return 
+    
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.minBitFlips(start=10, goal=7))
