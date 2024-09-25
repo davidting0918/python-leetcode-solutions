6232add019871de84dd8c0df182d944e4f2545ef
David Ding <davidding@kronosresearch.com>
2024-09-25 07:29:00 +0000

    add 2416 structure

---
+++ 2416-sum-of-prefix-scores-of-strings.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/sum-of-prefix-scores-of-strings/
+from typing import List
+class Solution:
+    def sumPrefixScores(self, words: List[str]) -> List[int]:
+        return
+    
+if __name__ == '__main__':
+    s = Solution()
