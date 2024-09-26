985dba878e455415ef15ed8fd3ec23ed7b37683e
David Ding <davidding@kronosresearch.com>
2024-09-26 01:59:00 +0000

    Add 729 structure

---
+++ 729-my-calendar-i.py
@@ -0,0 +1,16 @@
+# https://leetcode.com/problems/my-calendar-i/description/?envType=daily-question&envId=2024-09-26
+class MyCalendar:
+
+    def __init__(self):
+        pass
+
+    def book(self, start: int, end: int) -> bool:
+        return
+
+
+# Your MyCalendar object will be instantiated and called as such:
+if __name__ == "__main__":
+    start = 10
+    end = 20
+    obj = MyCalendar()
+    param_1 = obj.book(start,end)
