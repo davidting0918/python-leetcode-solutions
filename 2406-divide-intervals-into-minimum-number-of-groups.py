5ef8d15bda58706b50b008a916a44646b15e478a
davidting <david.ting@tksring.com>
2024-10-12 16:39:31 +0800

    Add 2406 note

--- 2406-divide-intervals-into-minimum-number-of-groups.py
+++ 2406-divide-intervals-into-minimum-number-of-groups.py
@@ -1,9 +1,9 @@
 # https://leetcode.com/problems/divide-intervals-into-minimum-number-of-groups/description/
 from typing import List
 
-
 class Solution:
     def minGroups(self, intervals: List[List[int]]) -> int:
+        # logic sort the start and end time, if any intervals overlap, there must be a new group
         start_times = sorted(i[0] for i in intervals)
         end_times = sorted(i[1] for i in intervals)
         end_ptr, group_count = 0, 0
