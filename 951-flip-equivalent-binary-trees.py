551079784b3b6e4929ccf5abdabd6e3dc1314916
davidting <david.ting@tksring.com>
2024-10-24 23:27:36 +0800

    Add 951 finished code

--- 951-flip-equivalent-binary-trees.py
+++ 951-flip-equivalent-binary-trees.py
@@ -9,7 +9,15 @@ class TreeNode:
         self.right = right
 class Solution:
     def flipEquiv(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        return
+        def flip(node1: Optional[TreeNode], node2: Optional[TreeNode]) -> bool:
+            if node1 is None and node2 is None:
+                return True
+            if node1 is None or node2 is None or node1.val != node2.val:
+                return False
+            return (flip(node1.left, node2.left) and flip(node1.right, node2.right)) or (flip(node1.left, node2.right) and flip(node1.right, node2.left))
+        return flip(root1, root2)
+
+
 
 if __name__ == "__main__":
-    pass 
+    pass
