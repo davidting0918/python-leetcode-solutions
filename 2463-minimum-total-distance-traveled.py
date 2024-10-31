eb52906eec665be57d83a97748cbfdb4edac3860
davidting <david.ting@tksring.com>
2024-10-31 12:19:50 +0800

    Add 2463 structure

---
+++ 2463-minimum-total-distance-traveled.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/minimum-total-distance-traveled/description/
+from typing import List
+
+class Solution:
+    def minimumTotalDistance(self, robot: List[int], factory: List[List[int]]) -> int:
+        sorted_robot = sorted(robot)
+
+        return
+
+
+if __name__ == "__main__":
+    s = Solution()
+    robot = [0, 4, 6]
+    factory = [[2,2],[6,2]]
