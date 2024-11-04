16b4b45742eab582ac8f9b50d48abae8c3b08e65
David Ding <davidding@kronosresearch.com>
2024-11-04 01:53:58 +0000

    Add 3163 structure

---
+++ 3163-string-compression-iii.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/string-compression-iii/description/?envType=daily-question&envId=2024-11-04
+class Solution:
+    def compressedString(self, word: str) -> str:
+        return
+    
+if __name__ == "__main__":
+    s = Solution()
+    word = "abcde"
+    print(s.compressedString(word))
