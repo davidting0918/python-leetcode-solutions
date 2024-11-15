61eb1c5037b0157edf95a4941761f53efe04b961
David Ding <davidding@kronosresearch.com>
2024-11-15 06:43:19 +0000

    Add 1574 structure

---
+++ 1574-shortest-subarray-to-be-removed-to-make-array-sorted.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/shortest-subarray-to-be-removed-to-make-array-sorted/description/
+from typing import List
+class Solution:
+    def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
+        return
+    
+if __name__ == "__main__":
+    s = Solution()
+    arr = [1,2,3,10,4,2,3,5]
+    print(s.findLengthOfShortestSubarray(arr))
