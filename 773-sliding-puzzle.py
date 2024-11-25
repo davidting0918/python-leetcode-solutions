9abe65d03238470ea22157b7304fbc557b51e8f3
David Ding <davidding@kronosresearch.com>
2024-11-25 08:44:28 +0000

    Add 773 structure

---
+++ 773-sliding-puzzle.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/sliding-puzzle/description/?envType=daily-question&envId=2024-11-25
+from typing import List
+
+class Solution:
+    def slidingPuzzle(self, board: List[List[int]]) -> int:
+        return
+if __name__ == "__main__":
+    s = Solution()
+    board = [[1,2,3],[4,0,5]]
+    print(s.slidingPuzzle(board))  # 1
