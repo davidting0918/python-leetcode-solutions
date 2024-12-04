8dc3223fcfb40685c5acbae17508bab8059128ee
davidting <david.ting@tksring.com>
2024-12-05 02:21:21 +0800

    Add 221 finished code

--- 221-maximal-square.py
+++ 221-maximal-square.py
@@ -2,7 +2,22 @@
 from typing import List
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-        return
+        dp = [
+            [0 for i in range(len(matrix[0]))] for j in range(len(matrix))
+        ]
+
+
+        for column in range(len(matrix[0])):
+            for row in range(len(matrix)):
+                if column == 0 or row == 0:
+                    dp[row][column] = int(matrix[row][column])
+                elif matrix[row][column] == "1":
+                    dp[row][column] = min(
+                        dp[row-1][column],
+                        dp[row][column-1],
+                        dp[row-1][column-1]
+                    ) + 1
+        return max(max(row) for row in dp) ** 2
 
 if __name__ == "__main__":
     s = Solution()
