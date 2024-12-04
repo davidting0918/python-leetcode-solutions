2753fa7a1ef752b4bcab92b2cb3a5f5be27f6236
David Ding <davidding@kronosresearch.com>
2024-12-04 08:04:53 +0000

    Add 2825 structure

---
+++ 2825-make-string-a-subsequence-using-cyclic-increments.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/make-string-a-subsequence-using-cyclic-increments/description/
+class Solution:
+    def canMakeSubsequence(self, str1: str, str2: str) -> bool:
+        return
+    
+if __name__ == "__main__":
+    s = Solution()
+    str1 = "abc"
+    str2 = "ad"
+    print(s.canMakeSubsequence(str1, str2))  # True
