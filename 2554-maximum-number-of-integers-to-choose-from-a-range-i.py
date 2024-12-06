313f874f4d9ea01b539e4ec7b688ed80473b9cf1
David Ding <davidding@kronosresearch.com>
2024-12-06 02:07:48 +0000

    Add 2554 structure

---
+++ 2554-maximum-number-of-integers-to-choose-from-a-range-i.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/maximum-number-of-integers-to-choose-from-a-range-i/description/?envType=daily-question&envId=2024-12-06
+from typing import List
+class Solution:
+    def maxCount(self, banned: List[int], n: int, maxSum: int) -> int:
+        return
+    
+if __name__ == "__main__":
+    s = Solution()
+    banned = [1,6,5]
+    n = 5
+    maxSum = 6
+    print(s.maxCount(banned, n, maxSum))
