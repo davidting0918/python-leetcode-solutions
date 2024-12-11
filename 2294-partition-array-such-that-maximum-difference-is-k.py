0b593d6b500d39db4d012b07341dc99005b9c085
davidting0918 <dting0918@gmail.com>
2024-12-11 21:51:51 +0800

    Add 2294 finished code

--- 2294-partition-array-such-that-maximum-difference-is-k.py
+++ 2294-partition-array-such-that-maximum-difference-is-k.py
@@ -4,7 +4,21 @@ from typing import List
 
 class Solution:
     def partitionArray(self, nums: List[int], k: int) -> int:
-        return
+        
+        nums.sort()
+        n = len(nums)
+        
+        dp = [0] * n
+        index = 0
+        dp[0] = nums[0]
+        for i in range(1, n):
+            if nums[i] - dp[index] > k:
+                index += 1
+                dp[index] = nums[i]
+            else:
+                dp[index] = min(dp[index], nums[i])
+        
+        return index + 1
     
 if __name__ == "__main__":
     s = Solution()
