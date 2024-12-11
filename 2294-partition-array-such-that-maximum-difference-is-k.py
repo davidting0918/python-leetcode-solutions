b1cee7b48417f98b78b0be81c1d761f8c3474e18
davidting0918 <dting0918@gmail.com>
2024-12-11 21:45:00 +0800

    Add 2294 structure

---
+++ 2294-partition-array-such-that-maximum-difference-is-k.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/partition-array-such-that-maximum-difference-is-k/
+from typing import List
+
+
+class Solution:
+    def partitionArray(self, nums: List[int], k: int) -> int:
+        return
+    
+if __name__ == "__main__":
+    s = Solution()
+    nums = [3,6,1,2,5]
+    k = 2
+    print(s.partitionArray(nums, k)) # 3
