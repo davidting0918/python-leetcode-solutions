c4718aca316394fcc2b6a3687a88657010676d3c
davidting <david.ting@tksring.com>
2024-12-11 16:24:39 +0800

    Add 2779 structure

---
+++ 2779-maximum-beauty-of-an-array-after-applying-operation.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/maximum-beauty-of-an-array-after-applying-operation/description/?envType=daily-question&envId=2024-12-11
+from typing import List
+
+
+class Solution:
+    def maximumBeauty(self, nums: List[int], k: int) -> int:
+        return
+
+
+if __name__ == "__main__":
+    s = Solution()
+    nums = [4, 6, 1, 2]
+    k = 2
+    print(s.maximumBeauty(nums, k)) # 13
