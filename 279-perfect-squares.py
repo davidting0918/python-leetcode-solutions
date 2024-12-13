be9d5b2a06edae14b2e0a73f1114ff7274a9bd0c
David Ding <davidding@kronosresearch.com>
2024-12-13 10:35:13 +0000

    Add 279 unfinished solution

--- 279-perfect-squares.py
+++ 279-perfect-squares.py
@@ -1,7 +1,22 @@
 # https://leetcode.com/problems/perfect-squares/description/?envType=study-plan-v2&envId=top-100-liked
+import math
+
 class Solution:
     def numSquares(self, n: int) -> int:
-        return 
+        dp = [
+            float("inf") for _ in range(n+1)
+        ]
+        dp[0] = 0
+        
+        for i in range(1, n+1):
+            # get the max square root of i
+            target = math.floor(i ** (1/2)) 
+            dp[i] = dp[i - target ** 2] + 1
+
+            continue
+            
+
+        return dp[-1]
     
 
 if __name__ == "__main__":
