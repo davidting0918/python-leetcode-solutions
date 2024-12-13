84e823d49ea5da80d9faa479030a2cb89bc5d316
David Ding <davidding@kronosresearch.com>
2024-12-13 07:26:19 +0000

    Add 1387 structure

---
+++ 1387-sort-integers-by-the-power-value.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/sort-integers-by-the-power-value/
+class Solution:
+    def getKth(self, lo: int, hi: int, k: int) -> int:
+        return
+    
+
+if __name__ == "__main__":
+    s = Solution()
+    lo = 12
+    hi = 15
+    k = 2
+    print(s.getKth(lo, hi, k)) # 13
