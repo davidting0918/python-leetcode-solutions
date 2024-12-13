87bd49f6a328957c682917999507acbc5813bca5
David Ding <davidding@kronosresearch.com>
2024-12-13 07:30:33 +0000

    Add 279 structure

---
+++ 279-perfect-squares.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/perfect-squares/description/?envType=study-plan-v2&envId=top-100-liked
+class Solution:
+    def numSquares(self, n: int) -> int:
+        return 
+    
+
+if __name__ == "__main__":
+    s = Solution()
+    n = 12
+    print(s.numSquares(n)) # 3
