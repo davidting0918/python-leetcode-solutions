c9ed28d4d5f9064be4823b3ddee3d7a295e71608
David Ding <davidding@kronosresearch.com>
2024-12-17 08:52:57 +0000

    Add 2182 structure

---
+++ 2182-construct-string-with-repeat-limit.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/construct-string-with-repeat-limit/description/?envType=daily-question&envId=2024-12-17
+class Solution:
+    def repeatLimitedString(self, s: str, repeatLimit: int) -> str:
+        return
+    
+
+if __name__ == "__main__":
+    s = Solution()
+    string = "cczazcc"
+    repeatLimit = 3
+    print(s.repeatLimitedString(string, repeatLimit))
