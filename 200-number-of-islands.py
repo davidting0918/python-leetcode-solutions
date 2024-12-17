08d9b777b7c06f50529150eb1a806c66157c773c
David Ding <davidding@kronosresearch.com>
2024-12-17 08:52:35 +0000

    Add 200 structure

---
+++ 200-number-of-islands.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/number-of-islands/?envType=study-plan-v2&envId=top-100-liked
+from typing import List
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        return
+
+
+if __name__ == "__main__":
+    s = Solution()
+    grid = [["1","1","1","1","0"],["1","1","0","1","0"],["1","1","0","0","0"],["0","0","0","0","0"]]
+    print(s.numIslands(grid)) # 1
