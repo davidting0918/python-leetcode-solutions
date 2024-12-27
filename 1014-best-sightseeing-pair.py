8c914dd549a2b0c8715f937ab9ed418c3993d75c
unknown <dting0918@gmail.com>
2024-12-28 00:50:00 +0800

    Add 1014 structure

---
+++ 1014-best-sightseeing-pair.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/best-sightseeing-pair/description/
+from typing import List
+
+
+class Solution:
+    def maxScoreSightseeingPair(self, values: List[int]) -> int:
+        return
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.maxScoreSightseeingPair([8,1,5,2,6])) # 11
