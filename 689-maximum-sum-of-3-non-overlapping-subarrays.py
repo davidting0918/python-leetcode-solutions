9ef3aceb843dce0739437f8473c5a0ad9781881a
unknown <dting0918@gmail.com>
2024-12-28 11:00:11 +0800

    Add 689 structure

---
+++ 689-maximum-sum-of-3-non-overlapping-subarrays.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/maximum-sum-of-3-non-overlapping-subarrays/description/?envType=daily-question&envId=2024-12-28
+from typing import List
+
+class Solution:
+    def maxSumOfThreeSubarrays(self, nums: List[int], k: int) -> List[int]:
+        return
+
+if __name__ == "__main__":
+    s = Solution()
+    nums = [1,2,1,2,6,7,5,1]
+    k = 2
+    print(s.maxSumOfThreeSubarrays(nums, k)) # [0, 3, 5]
