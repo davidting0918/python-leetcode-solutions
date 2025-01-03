27968378e7a839860120f0c1ba3ab39345735721
David Ding <davidding@kronosresearch.com>
2025-01-03 09:24:05 +0000

    Add 2270 structure

---
+++ 2270-number-of-ways-to-split-array.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/number-of-ways-to-split-array/description/?envType=daily-question&envId=2025-01-03
+from typing import List
+
+class Solution:
+    def waysToSplitArray(self, nums: List[int]) -> int:
+        return
+    
+if __name__ == "__main__":
+    s = Solution()
+    nums = [10,4,-8,7]
+    print(s.waysToSplitArray(nums)) # 2
