d4123b78c9900c81df95781111e7b6784c2f6272
unknown <dting0918@gmail.com>
2025-01-05 21:46:35 +0800

    Add 2381 finished code

--- solutions/2381-shifting-letters-ii.py
+++ solutions/2381-shifting-letters-ii.py
@@ -1,10 +1,33 @@
 # https://leetcode.com/problems/shifting-letters-ii/description/?envType=daily-question&envId=2025-01-05
+from turtledemo.penrose import start
 from typing import List
 
 
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
-        return
+        # use prefix sum to calculate the cum delta for each letter in each index so far
+        n = len(s)
+        delta = [0] * (n + 1)
+
+        for shift in shifts:
+            start, end, amount = shift
+            if amount == 0:
+                delta[start] -= 1
+                delta[end + 1] += 1
+            else:
+                delta[start] += 1
+                delta[end + 1] -= 1
+
+        cum_delta = 0
+        result = ""
+        for i in range(n):
+            cum_delta += delta[i]
+            result += self.get_new_letter(s[i], cum_delta)
+
+        return result
+
+    def get_new_letter(self, letter: str, shift: int) -> str:
+        return chr((ord(letter) - ord('a') + shift) % 26 + ord('a'))
 
 if __name__ == "__main__":
     s = Solution()
