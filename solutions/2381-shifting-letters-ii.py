642d21112584d1ffec7dd8ecab4ae518b9b72e21
unknown <dting0918@gmail.com>
2025-01-05 19:21:23 +0800

    Add 2381 structure

---
+++ solutions/2381-shifting-letters-ii.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/shifting-letters-ii/description/?envType=daily-question&envId=2025-01-05
+from typing import List
+
+
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        return
+
+if __name__ == "__main__":
+    s = Solution()
+    string = 'abc'
+    shifts = [[0,1,0],[1,2,1],[0,2,1]]
+    print(s.shiftingLetters(string, shifts)) # bcd
