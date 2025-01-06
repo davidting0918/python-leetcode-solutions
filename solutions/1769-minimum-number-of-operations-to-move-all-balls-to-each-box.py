2411f661e613106eec3a574b637607c7fa6b7513
David Ding <davidding@kronosresearch.com>
2025-01-06 03:07:10 +0000

    Add 1769 structure

---
+++ solutions/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/minimum-number-of-operations-to-move-all-balls-to-each-box/description/?envType=daily-question&envId=2025-01-06
+from typing import List
+
+class Solution:
+    def minOperations(self, boxes: str) -> List[int]:
+        return
+    
+if __name__ == "__main__":
+    s = Solution()
+    boxes = "110"
+    print(s.minOperations(boxes)) # [1,1,3]
