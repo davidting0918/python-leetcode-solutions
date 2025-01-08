081c2cbf942b71cb11f8ec5a7025b3e42db062e5
David Ding <davidding@kronosresearch.com>
2025-01-08 03:58:03 +0000

    Add 34 structure

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/?envType=study-plan-v2&envId=top-100-liked
+from typing import List
+
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        return
+    
+if __name__ == "__main__":
+    s = Solution()
+    nums = [5,7,7,8,8,10]
+    target = 8
+    print(s.searchRange(nums, target))
