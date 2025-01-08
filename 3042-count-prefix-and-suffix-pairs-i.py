3573a65e95a68cca32dc6e6bb8037f15ae7ac715
David Ding <davidding@kronosresearch.com>
2025-01-08 03:48:32 +0000

    Add 3042 finished code

--- 3042-count-prefix-and-suffix-pairs-i.py
+++ 3042-count-prefix-and-suffix-pairs-i.py
@@ -3,9 +3,21 @@ from typing import List
 
 class Solution:
     def countPrefixSuffixPairs(self, words: List[str]) -> int:
-        return
+        answer = 0
+        n = len(words)
+
+        for i in range(n):
+            for j in range(i+1, n):
+                if words[j].startswith(words[i]):
+                    answer += 1
+                    continue
+                
+                if words[j].endswith(words[i]):
+                    answer += 1
+                    continue
+        return answer
     
 if __name__ == "__main__":
     s = Solution()
-    words = ["a","aba","ababa","aa"]
+    words = ["abab","ab"]
     print(s.countPrefixSuffixPairs(words)) # 4
