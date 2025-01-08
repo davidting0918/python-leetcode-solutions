d662d4edadabd58288ae04824247f8053b6ae7b6
David Ding <davidding@kronosresearch.com>
2025-01-08 02:41:39 +0000

    Add 3042 structure

---
+++ 3042-count-prefix-and-suffix-pairs-i.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/count-prefix-and-suffix-pairs-i/description/?envType=daily-question&envId=2025-01-08
+from typing import List
+
+class Solution:
+    def countPrefixSuffixPairs(self, words: List[str]) -> int:
+        return
+    
+if __name__ == "__main__":
+    s = Solution()
+    words = ["a","aba","ababa","aa"]
+    print(s.countPrefixSuffixPairs(words)) # 4
