da5d743186fb59c3a98f83c2e782c9b80905765e
unknown <dting0918@gmail.com>
2025-01-13 00:23:05 +0800

    Add 518 structure

---
+++ solutions/518-coin-change-ii.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/problems/coin-change-ii/description/?envType=study-plan-v2&envId=dynamic-programming
+
+from typing import List
+
+
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        return
+
+if __name__ == "__main__":
+    s = Solution()
+    amount = 5
+    coins = [1, 2, 5]
+    print(s.change(amount, coins)) # 4
