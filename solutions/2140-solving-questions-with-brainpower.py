d9e54680b228445e60708be5100be35209640903
unknown <dting0918@gmail.com>
2025-01-13 00:25:34 +0800

    Add 2140 structure

---
+++ solutions/2140-solving-questions-with-brainpower.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/solving-questions-with-brainpower/description/?envType=study-plan-v2&envId=dynamic-programming
+from typing import List
+
+
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        return
+
+if __name__ == "__main__":
+    s = Solution()
+    questions = [[1, 2], [2, 3], [3, 4]]
+    print(s.mostPoints(questions)) # 3
