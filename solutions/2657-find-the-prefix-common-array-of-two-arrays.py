170bb5edf9071f03745e1625e8626b46bace3dcc
David Ding <davidding@kronosresearch.com>
2025-01-14 03:11:36 +0000

    refactor solution path

---
+++ solutions/2657-find-the-prefix-common-array-of-two-arrays.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/find-the-prefix-common-array-of-two-arrays/?envType=daily-question&envId=2025-01-14
+from typing import List
+
+class Solution:
+    def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
+        return
+    
+if __name__ == '__main__':
+    s = Solution()
+    A = [1,3,2,4]
+    B = [3,1,2,4]
+    print(s.findThePrefixCommonArray(A, B)) # [1,2,4]
