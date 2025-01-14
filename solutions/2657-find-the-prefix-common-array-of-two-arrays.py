591453d70d691584a314b176a0f48d38e1869e54
unknown <dting0918@gmail.com>
2025-01-14 20:00:48 +0800

    Add 2657 finished code

--- solutions/2657-find-the-prefix-common-array-of-two-arrays.py
+++ solutions/2657-find-the-prefix-common-array-of-two-arrays.py
@@ -3,7 +3,14 @@ from typing import List
 
 class Solution:
     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
-        return
+        n = len(A)
+
+        answer = []
+        for i in range(n):
+            answer.append(
+                i + 1 - len(set(B[:i+1]) - set(A[:i+1]))
+            )
+        return answer
     
 if __name__ == '__main__':
     s = Solution()
