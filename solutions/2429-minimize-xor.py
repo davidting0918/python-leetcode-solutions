57edd9cbc119e4e5e28a4c61f42bfb895ef6c32b
David Ding <davidding@kronosresearch.com>
2025-01-15 02:17:55 +0000

    Add 2429 structure

---
+++ solutions/2429-minimize-xor.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/minimize-xor/description/?envType=daily-question&envId=2025-01-15
+
+
+class Solution:
+    def minimizeXor(self, num1: int, num2: int) -> int:
+        return
+
+
+if __name__ == "__main__":
+    s = Solution()
+    num1 = 3
+    num2 = 5
+    print(s.minimizeXor(num1, num2))  
