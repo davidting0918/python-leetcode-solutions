62a071d2b776ba42ccbfe04debf93bdb70574da3
David Ding <davidding@kronosresearch.com>
2025-01-15 10:09:03 +0000

    add 2420 code

--- solutions/2429-minimize-xor.py
+++ solutions/2429-minimize-xor.py
@@ -3,6 +3,11 @@
 
 class Solution:
     def minimizeXor(self, num1: int, num2: int) -> int:
+        # the answer should have the same 1 bits as num2 and num1 XOR answer should be min
+        num2_bit_count = bin(num2).count('1')
+
+        num1_bin = bin(num1)[2:]
+        additional_one = max(0, num2_bit_count - num1_bin.count('1'))
         return
 
 
