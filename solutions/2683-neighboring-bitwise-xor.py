8d3accdf20983f312f7d92a2598a24b76008e57f
unknown <dting0918@gmail.com>
2025-01-17 11:26:38 +0800

    Add 2683 structure

---
+++ solutions/2683-neighboring-bitwise-xor.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/neighboring-bitwise-xor/description/?envType=daily-question&envId=2025-01-17
+from typing import List
+
+class Solution:
+    def doesValidArrayExist(self, derived: List[int]) -> bool:
+        return
+
+if __name__ == '__main__':
+    s = Solution()
+    derived = [1,1,0]
+    print(s.doesValidArrayExist(derived)) # True
