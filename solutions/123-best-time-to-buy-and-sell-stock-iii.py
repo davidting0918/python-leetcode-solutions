cf6ce53e0bd3241e0b133bdce787f585d9e9dad1
David Ding <davidding@kronosresearch.com>
2025-01-17 07:13:21 +0000

    Add 123 structure

---
+++ solutions/123-best-time-to-buy-and-sell-stock-iii.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/description/?envType=study-plan-v2&envId=dynamic-programming
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        return
+    
+if __name__ == '__main__':
+    s = Solution()
+    prices = [3,3,5,0,0,3,1,4]
+    print(s.maxProfit(prices)) # 6
