475317ff7c46246c90aa79c9d3cf41c4dcaadfe0
David Ding <davidding@kronosresearch.com>
2025-01-20 10:34:15 +0000

    Add 2661 structure

---
+++ solutions/2661-first-completely-painted-row-or-column.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/first-completely-painted-row-or-column/description/?envType=daily-question&envId=2025-01-20
+from typing import List
+
+class Solution:
+    def firstCompleteIndex(self, arr: List[int], mat: List[List[int]]) -> int:
+        return
+    
+if __name__ == '__main__':
+    s = Solution()
+    arr = [1,2,3,4]
+    mat = [[1, 4], [2, 3]]
+    print(s.firstCompleteIndex(arr, mat)) # 1
