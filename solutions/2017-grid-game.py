9f37b4af356c6114377b99bf54778ebe0cba0f59
unknown <dting0918@gmail.com>
2025-01-21 11:49:29 +0800

    Add 2017 structure

---
+++ solutions/2017-grid-game.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/minimum-cost-to-make-at-least-one-valid-path-in-a-grid/?envType=daily-question&envId=2025-01-18
+from typing import List
+
+class Solution:
+    def gridGame(self, grid: List[List[int]]) -> int:
+        return
+
+if __name__ == "__main__":
+    s = Solution()
+    grid = [[2,5,4],[1,5,1]]
+    print(s.gridGame(grid))
