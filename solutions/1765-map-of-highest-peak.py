fabde0655b287169ab7dd4e2fa46a1c0618d0a2f
unknown <dting0918@gmail.com>
2025-01-22 23:19:44 +0800

    Add 1765 unfinished code

--- solutions/1765-map-of-highest-peak.py
+++ solutions/1765-map-of-highest-peak.py
@@ -4,7 +4,25 @@ from typing import List
 
 class Solution:
     def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
-        return
+        waters = []
+
+        n = len(isWater)
+        m = len(isWater[0])
+
+        for i in range(n):
+            for j in range(m):
+                if isWater[i][j] == 1:
+                    waters.append((i, j))
+
+        answer = [[float("inf")] * m for _ in range(n)]
+        for i in range(n):
+            for j in range(m):
+                if (i, j) in waters:
+                    answer[i][j] = 0
+                    continue
+                for water_x, water_y in waters:
+                    answer[i][j] = min(answer[i][j], abs(i - water_x) + abs(j - water_y))
+        return answer
 
 if __name__ == "__main__":
     s = Solution()  
