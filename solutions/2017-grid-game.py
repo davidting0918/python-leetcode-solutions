72ee61261b37e99d0890a2ebe2f1162a8d8dfd04
unknown <dting0918@gmail.com>
2025-01-22 22:36:24 +0800

    Add 2017 structure

--- solutions/2017-grid-game.py
+++ solutions/2017-grid-game.py
@@ -3,6 +3,17 @@ from typing import List
 
 class Solution:
     def gridGame(self, grid: List[List[int]]) -> int:
+        # since there are only 2 rows, so just need to decide when the first robot need to move to the second row.
+        # use the prefix sum of the 2 rows to solve this problem
+        rows = 2
+        cols = len(grid[0])
+
+        prefix1 = [0] * (cols + 1)
+        prefix2 = [0] * (cols + 1)
+
+        for i in range(cols):
+            prefix1[i + 1] = prefix1[i] + grid[0][i]
+            prefix2[i + 1] = prefix2[i] + grid[1][i]
         return
 
 if __name__ == "__main__":
