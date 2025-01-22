d2b27f2f0aedd3e4c284b4e99e4c676573afda1a
David Ding <davidding@kronosresearch.com>
2025-01-22 02:59:50 +0000

    Add 1765 structure

---
+++ solutions/1765-map-of-highest-peak.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/map-of-highest-peak/description/?envType=daily-question&envId=2025-01-22
+
+from typing import List
+
+class Solution:
+    def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
+        return
+
+if __name__ == "__main__":
+    s = Solution()  
+    iswater = [[0,1],[0,0]]
+    print(s.highestPeak(iswater))
