cf949bfd4f32014d4d5a5ffde37dce460b122108
David Ding <davidding@kronosresearch.com>
2025-01-23 07:54:06 +0000

    Add 1267 finished code

--- solutions/1267-count-servers-that-communicate.py
+++ solutions/1267-count-servers-that-communicate.py
@@ -3,10 +3,28 @@ from typing import List
 
 class Solution:
     def countServers(self, grid: List[List[int]]) -> int:
-        return
-    
+        # count all isolated servers
+        answer = 0
+        n = len(grid)
+        m = len(grid[0])
+
+        rows = [
+            sum(grid[i])
+            for i in range(n)
+        ]
+
+        cols = [
+            sum(grid[i][j] for i in range(n))
+            for j in range(m)
+        ]
+
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 1 and (rows[i] > 1 or cols[j] > 1):
+                    answer += 1
+        return answer
 
 if __name__ == "__main__":
     s = Solution()
-    grid = [[1,0],[0,1]]
+    grid = [[1, 0, 1], [0, 0, 0], [0, 0, 1]]
     print(s.countServers(grid)) # 0
