e226596da6461b05f16b1573b5b71cd44944b584
David Ding <davidding@kronosresearch.com>
2025-01-23 07:26:30 +0000

    Add 1267 structure

---
+++ solutions/1267-count-servers-that-communicate.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/count-servers-that-communicate/description/?envType=daily-question&envId=2025-01-23
+from typing import List
+
+class Solution:
+    def countServers(self, grid: List[List[int]]) -> int:
+        return
+    
+
+if __name__ == "__main__":
+    s = Solution()
+    grid = [[1,0],[0,1]]
+    print(s.countServers(grid)) # 0
