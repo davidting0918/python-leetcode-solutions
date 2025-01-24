e014c87d95152e1c33326405a7fec746dfeeda77
David Ding <davidding@kronosresearch.com>
2025-01-24 09:40:32 +0000

    Add 802 structure

---
+++ solutions/802-find-eventual-safe-states.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/find-eventual-safe-states/description/?envType=daily-question&envId=2025-01-24
+from typing import List
+
+class Solution:
+    def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
+        return
+    
+if __name__ == "__main__":
+    s = Solution()
+    graph = [[1,2],[2,3],[5],[0],[5],[],[]]
+    print(s.eventualSafeNodes(graph))
