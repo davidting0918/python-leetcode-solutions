af65b4840acc3b388cdc4ec5e43474e40eb569f3
unknown <dting0918@gmail.com>
2025-01-25 23:17:59 +0800

    Add 2948 structure

---
+++ solutions/2948-find-eventual-safe-states.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/find-eventual-safe-states/submissions/1520111571/?envType=daily-question&envId=2025-01-25
+from typing import List
+
+class Solution:
+    def lexicographicallySmallestArray(self, nums: List[int], limit: int) -> List[int]:
+        return
+
+if __name__ == "__main__":
+    s = Solution()
+    nums = [1,5,3,9,8]
+    limit = 2
+    print(s.lexicographicallySmallestArray(nums, limit)) # [1,5,3,9,8]
