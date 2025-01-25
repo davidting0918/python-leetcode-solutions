891833f2d563d1c025b2c67d3477ec550735c33d
unknown <dting0918@gmail.com>
2025-01-25 23:13:36 +0800

    Add 802 finished code

--- solutions/802-find-eventual-safe-states.py
+++ solutions/802-find-eventual-safe-states.py
@@ -3,7 +3,29 @@ from typing import List
 
 class Solution:
     def eventualSafeNodes(self, graph: List[List[int]]) -> List[int]:
-        return
+        # use reversed graph to check if a node is safe, and the neighbors of a node is safe
+        n = len(graph)
+
+        reversed_graph = [[] for _ in range(n)]
+
+        for i in range(n):
+            for j in graph[i]:
+                reversed_graph[j].append(i)
+
+        degrees = [len(graph[i]) for i in range(n)]
+        queue = [i for i in range(n) if not graph[i]]
+        answers = []
+        while queue:
+            node = queue.pop(0)
+            answers.append(node)
+            for i in reversed_graph[node]:
+                degrees[i] -= 1
+                if degrees[i] == 0:
+                    queue.append(i)
+            continue
+
+
+        return sorted(answers)
     
 if __name__ == "__main__":
     s = Solution()
