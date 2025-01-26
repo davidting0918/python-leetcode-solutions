7c46685883b89626e12aa86b39b3d32d0de8a0ce
unknown <dting0918@gmail.com>
2025-01-26 14:39:19 +0800

    Add 13 finished code

--- Weekly Contest 434/maximum-frequency-after-subarray-operation.py
+++ Weekly Contest 434/maximum-frequency-after-subarray-operation.py
@@ -5,22 +5,26 @@ class Solution:
     def maxFrequency(self, nums: List[int], k: int) -> int:
         # find nums == k
         n = len(nums)
+        last_pos = {}
+        res = [0 for i in range(n)]
+        k_count = [0 for i in range(n+1)]
 
-        num_count = {
-            num: 0 for num in set(nums)
-        }
-        num_count[k] = 0
+        for i, num in enumerate(nums):
+            k_count[i+1] = k_count[i] + (1 if num == k else 0)
+            res[i] = k_count[i]
 
-        dp = [0] * n
-        dp[0] = 1
-        num_count[nums[0]] += 1
-        for i in range(1, n):
-            num_count[nums[i]] += 1
-            dp[i] = max(dp[i-1], num_count[nums[i]] + num_count[k])
-        return dp[-1]
+            if num in last_pos:
+                res[i] = max(res[i], res[last_pos[num]] + 1)
+            last_pos[num] = i
+
+            continue
+        for i in range(n):
+            res[i] += (k_count[-1] - k_count[i+1])
+        return max(res)
+
 
 if __name__ == "__main__":
     s = Solution()
-    nums = [6, 1, 6]
+    nums = [1, 6, 6, 1, 6, 1]
     k = 1
     print(s.maxFrequency(nums, k))
