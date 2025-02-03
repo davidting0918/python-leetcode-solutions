8bfb5d8aec3d61cf894638504fff1319d1572839
David Ding <davidding@kronosresearch.com>
2025-02-03 06:37:19 +0000

    Add 3105 structure

---
+++ solutions/3105-longest-strictly-increasing-or-strictly-decreasing-subarray.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/longest-strictly-increasing-or-strictly-decreasing-subarray/description/?envType=daily-question&envId=2025-02-03
+
+from typing import List
+
+class Solution:
+    def longestMonotonicSubarray(self, nums: List[int]) -> int:
+        return
+
+if __name__ == "__main__":
+    s = Solution()
+    nums = [1,4,3,3,2]
+    print(s.longestMonotonicSubarray(nums))
