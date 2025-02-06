7ebb8620eb445185c5c65b850dcdc8bf2d1970c6
David Ding <davidding@kronosresearch.com>
2025-02-06 03:46:24 +0000

    Add 1726 structure

---
+++ 1726-tuple-with-same-product.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/tuple-with-same-product/description/?envType=daily-question&envId=2025-02-06
+
+from typing import List
+
+class Solution:
+    def tupleSameProduct(self, nums: List[int]) -> int:
+        return
+    
+if __name__ == '__main__':
+    s = Solution()
+    nums = [2,3,4,6]
+    print(s.tupleSameProduct(nums))
