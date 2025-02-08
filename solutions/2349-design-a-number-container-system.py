cf6a3dab30ff7a335a3ccef824163e871b1082d6
unknown <dting0918@gmail.com>
2025-02-08 21:53:30 +0800

    Add 2349 finished code

--- solutions/2349-design-a-number-container-system.py
+++ solutions/2349-design-a-number-container-system.py
@@ -1,16 +1,37 @@
 # https://leetcode.com/problems/design-a-number-container-system/description/
 
+import collections
+from sortedcontainers import SortedSet
+
 class NumberContainers:
 
     def __init__(self):
-        pass
+        self.nums = {}  # key: index, value: num
+        self.index = collections.defaultdict(SortedSet)  # key: num, index: heap of index
 
     def change(self, index: int, number: int) -> None:
-        pass
+        if index in self.nums:
+            old_num = self.nums[index]
+            self.index[old_num].remove(index)
+            if not self.index[old_num]:
+                del self.index[old_num]
+
+        self.nums[index] = number
+        self.index[number].add(index)
 
     def find(self, number: int) -> int:
-        pass
-# Your NumberContainers object will be instantiated and called as such:
-# obj = NumberContainers()
-# obj.change(index,number)
-# param_2 = obj.find(number)
+        if number not in self.index or not self.index[number]:
+            return -1
+        else:
+            return self.index[number][0]
+
+if __name__ == "__main__":
+    s = NumberContainers()
+    actions = ["find","change","change","change","change","find","change","find"]
+    values = [[10],[2,10],[1,10],[3,10],[5,10],[10],[1,20],[10]]
+
+    for i, action in enumerate(actions):
+        if action == "find":
+            print(s.find(values[i][0]))
+        else:
+            print(s.change(values[i][0], values[i][1]))
