ff87d7bea223a6ddeadd02bb0cdc783f43ccabc4
David Ding <davidding@kronosresearch.com>
2025-02-10 06:41:20 +0000

    Add 3174 structure

---
+++ solutions/3174-clear-digits.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/clear-digits/description/?envType=daily-question&envId=2025-02-10
+
+class Solution:
+    def clearDigits(self, s: str) -> str:
+        return
+    
+if __name__ == "__main__":
+    s = Solution()
+    string = 'abc'
+    print(s.clearDigits(string))
