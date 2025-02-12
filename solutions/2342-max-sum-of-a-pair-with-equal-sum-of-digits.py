2578aebdfb696421a87eb4f5451be4da0b831a2c
David Ding <davidding@kronosresearch.com>
2025-02-12 02:41:02 +0000

    Add 2342 finished code

--- solutions/2342-max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ solutions/2342-max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -3,7 +3,29 @@ from typing import List
 
 class Solution:
     def maximumSum(self, nums: List[int]) -> int:
-        return
+        n = len(nums)
+
+        hash_table = {}
+
+        for i in range(n):
+            digit_sum = sum(
+                [int(d) for d in str(nums[i])]
+            )
+            if digit_sum not in hash_table:
+                hash_table[digit_sum] = []
+
+            hash_table[digit_sum].append(nums[i])
+        
+        answer = 0
+        for i in hash_table:
+            if len(hash_table[i]) < 2:
+                continue
+            hash_table[i].sort()
+            answer = max(
+                answer,
+                hash_table[i][-1] + hash_table[i][-2]
+            )
+        return answer
     
 if __name__ == '__main__':
     s = Solution()
