925caa7a3c9b9cd14a05fe29c31a665ab9ecaf2b
David Ding <davidding@kronosresearch.com>
2025-02-12 02:25:14 +0000

    Add 2342 structure

---
+++ solutions/2342-max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/max-sum-of-a-pair-with-equal-sum-of-digits/description/?envType=daily-question&envId=2025-02-12
+from typing import List
+
+class Solution:
+    def maximumSum(self, nums: List[int]) -> int:
+        return
+    
+if __name__ == '__main__':
+    s = Solution()
+    nums = [18,43,36,13,7]
+    print(s.maximumSum(nums))
