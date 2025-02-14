4861e81f86768b3247cd3570ddc8c292a2918483
David Ding <davidding@kronosresearch.com>
2025-02-14 02:10:42 +0000

    Add 1352 structure

---
+++ solutions/1352-product-of-the-last-k-numbers.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/product-of-the-last-k-numbers/description/?envType=daily-question&envId=2025-02-14
+
+class ProductOfNumbers:
+
+    def __init__(self):
+        return        
+
+    def add(self, num: int) -> None:
+        return
+
+    def getProduct(self, k: int) -> int:
+        return
+
+
+# Your ProductOfNumbers object will be instantiated and called as such:
+# obj = ProductOfNumbers()
+# obj.add(num)
+# param_2 = obj.getProduct(k)
+
+
