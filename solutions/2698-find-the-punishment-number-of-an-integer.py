10959ed446c0320644801bff867b277806d21e98
unknown <dting0918@gmail.com>
2025-02-15 14:53:12 +0800

    Add 2698 sturcutre

---
+++ solutions/2698-find-the-punishment-number-of-an-integer.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/find-the-punishment-number-of-an-integer/description/?envType=daily-question&envId=2025-02-15
+class Solution:
+    def punishmentNumber(self, n: int) -> int:
+        return
+
+
+if __name__ == "__main__":
+    s = Solution()
+    n = 10
+    print(s.punishmentNumber(n))
