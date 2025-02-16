b2dfe37066942e993f07b9a505b15a29f887baa1
unknown <dting0918@gmail.com>
2025-02-16 21:20:24 +0800

    Add 1718 finished code

--- solutions/1718-construct-the-lexicographically-largest-valid-sequence.py
+++ solutions/1718-construct-the-lexicographically-largest-valid-sequence.py
@@ -3,22 +3,35 @@ from typing import List
 
 class Solution:
     def constructDistancedSequence(self, n: int) -> List[int]:
-        total_length = 2 * n - 1
+        def constructDistancedSequence(self, n: int) -> List[int]:
+            len_seq = 2 * n - 1  # 1 occurs once, 2-n appear twice
+            seq = [0] * len_seq  # init with empty vals
+            used = set()  # tracks nums used in seq
 
-        answer = [0] * total_length
+            def backtrack(i):
+                if i == len_seq: return True  # filled all of sequence
+                if seq[i]: return backtrack(i + 1)  # already filled index, check next
 
-        for num in range(n, 1, -1):
-            for i in range(2 * n - 1):
-                if answer[i] == 0:
-                    next_index = i + num
-                    if next_index >= total_length:
-                        break
-                    if answer[next_index] == 0:
-                        answer[i] = num
-                        answer[next_index] = num
-                        break
+                for num in range(n, 0, -1):  # try using num at index, n to 1 for lexicographically largest seq
+                    if num in used: continue  # already used num in sequence
 
-        return [i if i != 0 else 1 for i in answer]
+                    nxt = i + num if num > 1 else i  # second occurance of num, nxt = i if num = 1 so 1 occurance
+
+                    if nxt >= len_seq or seq[nxt] != 0: continue  # invalid second index
+
+                    seq[i] = seq[nxt] = num  # set two occurances of num
+                    used.add(num)
+
+                    if backtrack(i + 1):  # try to fill rest of sequence
+                        return True
+
+                    seq[i] = seq[nxt] = 0  # couldn't fill rest, reset seq
+                    used.remove(num)
+
+                return False  # no num option lead to valid sequence
+
+            backtrack(0)  # start backtrack at 0 index
+            return seq
 
 if __name__ == '__main__':
     s = Solution()
