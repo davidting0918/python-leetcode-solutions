d0c069b177d07aea910b59a25d4c3610bcc75bfc
David Ding <davidding@kronosresearch.com>
2025-02-26 07:35:46 +0000

    Add 1749 structure

---
+++ solutions/1749-maximum-absolute-sum-of-any-subarray.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/maximum-absolute-sum-of-any-subarray/description/?envType=daily-question&envId=2025-02-26
+from typing import List
+
+
+class Solution:
+    def maxAbsoluteSum(self, nums: List[int]) -> int:
+        return
+
+if __name__ == "__main__":
+    s = Solution()
+    nums = [1,-3,2,3,-4]
+    print(s.maxAbsoluteSum(nums))
