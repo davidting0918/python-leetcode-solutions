bf5e5557ec2e35524d299dc549db766e920f201e
unknown <dting0918@gmail.com>
2025-02-27 22:25:05 +0800

    Add 1079 structure

---
+++ solutions/1079-letter-tile-possibilities.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/letter-tile-possibilities/description/?envType=daily-question&envId=2025-02-17
+class Solution:
+    def numTilePossibilities(self, tiles: str) -> int:
+        return
+
+if __name__ == "__main__":
+    s = Solution()
+    tiles = "AAB"
+    print(s.numTilePossibilities(tiles))
