e8a3dbbd5962ce2ca6750c8d5756a37b20162477
David Ding <davidding@kronosresearch.com>
2025-02-27 10:00:16 +0000

    Add 873 structure

---
+++ solutions/873-length-of-longest-fibonacci-subsequence.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/length-of-longest-fibonacci-subsequence/description/?envType=daily-question&envId=2025-02-27
+from typing import List
+
+class Solution:
+    def lenLongestFibSubseq(self, arr: List[int]) -> int:
+        return
+    
+if __name__ == "__main__":
+    s = Solution()
+    arr = [1,2,3,4,5,6,7,8]
+    print(s.lenLongestFibSubseq(arr))
+
