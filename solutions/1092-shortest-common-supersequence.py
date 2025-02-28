35e1626cdd36a97ab0d4a293e49d78be8ff80d30
unknown <dting0918@gmail.com>
2025-02-28 11:03:03 +0800

    Add 1092 structure

---
+++ solutions/1092-shortest-common-supersequence.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/shortest-common-supersequence/?envType=daily-question&envId=2025-02-28
+
+class Solution:
+    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+        return
+
+
+if __name__ == "__main__":
+    s = Solution()
+    str1 = "abac"
+    str2 = "cab"
+    print(s.shortestCommonSupersequence(str1, str2))
