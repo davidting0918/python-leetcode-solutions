3f09148e134a0663f19931e6cc1fbb573d956aa0
David Ding <davidding@kronosresearch.com>
2025-03-03 01:54:59 +0000

    Add 2161 structure

---
+++ solutions/2161-partition-array-according-to-given-pivot.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/partition-array-according-to-given-pivot/description/?envType=daily-question&envId=2025-03-03
+from typing import List
+
+class Solution:
+    def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
+        return
+    
+if __name__ == "__main__":
+    s = Solution()
+    nums = [9, 12, 3, 5, 14, 10, 10]
+    pivot = 10
+    print(s.pivotArray(nums, pivot))
