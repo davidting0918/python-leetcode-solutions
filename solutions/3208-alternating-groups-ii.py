d12e3dd216cdf8e13e914e43b5e60793c4cb6df0
unknown <dting0918@gmail.com>
2025-03-09 22:39:12 +0800

    Add 3208 finished code

--- solutions/3208-alternating-groups-ii.py
+++ solutions/3208-alternating-groups-ii.py
@@ -3,7 +3,18 @@ from typing import List
 
 class Solution:
     def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
-        return
+        colors.extend(colors[:(k-1)])
+        count = 0
+        left = 0
+
+        for right in range(len(colors)): # make sure every subarray is a new one
+            if right > 0 and colors[right] == colors[right-1]:
+               # reset left and right
+               left = right
+
+            if right - left + 1 >= k:  # no need to make sure every subarray length is k
+                count += 1
+        return count
 
 
 if __name__ == "__main__":
