467e3245040cfbd445969c49bcddaaa27894d606
unknown <dting0918@gmail.com>
2025-03-09 21:20:49 +0800

    Add 3208 structure

---
+++ solutions/3208-alternating-groups-ii.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/alternating-groups-ii/?envType=daily-question&envId=2025-03-09
+from typing import List
+
+class Solution:
+    def numberOfAlternatingGroups(self, colors: List[int], k: int) -> int:
+        return
+
+
+if __name__ == "__main__":
+    s = Solution()
+    color = [0,1,0,1,0]
+    k = 3
+    print(s.numberOfAlternatingGroups(color, k))
