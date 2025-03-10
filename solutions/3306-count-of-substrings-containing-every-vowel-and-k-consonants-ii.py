d9dbdf5a6e7d5a0b160bb89647015f7df5e380ff
unknown <dting0918@gmail.com>
2025-03-10 20:16:08 +0800

    Add 3306 structure

---
+++ solutions/3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/count-of-substrings-containing-every-vowel-and-k-consonants-ii/description/?envType=daily-question&envId=2025-03-10
+
+class Solution:
+    def countOfSubstrings(self, word: str, k: int) -> int:
+        return
+    
+if __name__ == "__main__":
+    s = Solution()
+    word = "aeioqq"
+    k = 1
+    print(s.countOfSubstrings(word, k))
+
