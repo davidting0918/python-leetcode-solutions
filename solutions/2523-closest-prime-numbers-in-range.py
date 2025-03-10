27ec2d5c9690b92f1f2c80a575fce3669cf2929c
David Ding <davidding@kronosresearch.com>
2025-03-10 07:52:11 +0000

    Add 2523 structure

---
+++ solutions/2523-closest-prime-numbers-in-range.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/closest-prime-numbers-in-range/description/
+from typing import List
+
+class Solution:
+    def closestPrimes(self, left: int, right: int) -> List[int]:
+        return
+    
+
+if __name__ == "__main__":
+    s = Solution()
+    left = 10
+    right = 19
+    print(s.closestPrimes(left, right))
