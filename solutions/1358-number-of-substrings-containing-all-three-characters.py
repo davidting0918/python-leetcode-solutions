08e237c1981a07bae9726ed5725bbb79d8f33e36
unknown <dting0918@gmail.com>
2025-03-12 20:39:16 +0800

    Add 1358 structure

---
+++ solutions/1358-number-of-substrings-containing-all-three-characters.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/number-of-substrings-containing-all-three-characters/description/?envType=daily-question&envId=2025-03-11
+class Solution:
+    def numberOfSubstrings(self, s: str) -> int:
+        return
+    
+if __name__ == "__main__":
+    s = Solution()
+    print(s.numberOfSubstrings("abcabc"))
