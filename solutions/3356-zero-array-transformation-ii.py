61bdbd8171eff4dbc557309d81f46cac5ddbcd13
David Ding <davidding@kronosresearch.com>
2025-03-13 02:40:12 +0000

    Add 3356 structure

---
+++ solutions/3356-zero-array-transformation-ii.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/zero-array-transformation-ii/description/?envType=daily-question&envId=2025-03-13 
+from typing import List
+
+class Solution:
+    def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
+        return
+
+if __name__ == "__main__":
+    s = Solution()
+    nums = [2, 0, 2]
+    queries = [[0,2,1],[0,2,1],[1,1,3]]
+    print(s.minZeroArray(nums, queries))
