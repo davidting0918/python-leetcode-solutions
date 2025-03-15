56488f9855f85484a0cbd814dc0c4c5454eb4eb9
unknown <dting0918@gmail.com>
2025-03-15 14:55:40 +0800

    Add 2560 structure

---
+++ solutions/2560-house-robber-iv.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/house-robber-iv/description/?envType=daily-question&envId=2025-03-15
+from typing import List
+
+class Solution:
+    def minCapability(self, nums: List[int], k: int) -> int:
+        return
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.minCapability([2,3,5,9], 2))
