283a9d0b0351edd7753fe17237a3b3cdd8a85a89
David Ding <davidding@kronosresearch.com>
2025-03-17 03:22:35 +0000

    Add 2206 structure

---
+++ solutions/2206-divide-array-into-equal-pairs.py
@@ -0,0 +1,11 @@
+# https://leetcode.com/problems/divide-array-into-equal-pairs/description/?envType=daily-question&envId=2025-03-17
+
+
+class Solution:
+    def divideArray(self, nums: List[int]) -> bool:
+        return
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.divideArray([3, 2, 3, 2, 2, 2]))
