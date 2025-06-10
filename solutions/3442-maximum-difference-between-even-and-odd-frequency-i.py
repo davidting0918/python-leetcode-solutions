fbbc74789f36e46d73ebb0aa0b72243f43988257
unknown <dting0918@gmail.com>
2025-06-10 13:10:46 +0800

    Add 3442 finished code

---
+++ solutions/3442-maximum-difference-between-even-and-odd-frequency-i.py
@@ -0,0 +1,23 @@
+# https://leetcode.com/problems/maximum-difference-between-even-and-odd-frequency-i/description/?envType=daily-question&envId=2025-06-10
+
+class Solution:
+    def maxDifference(self, s: str) -> int:
+        letters = set(s)
+
+        even_min = float('inf')
+        odd_max = 0
+
+        for letter in letters:
+            letter_count = s.count(letter)
+            print(letter, letter_count)
+            if letter_count % 2 == 0:
+                even_min = min(even_min, letter_count)
+            else:
+                odd_max = max(odd_max, letter_count)
+        print(odd_max, even_min)
+        return odd_max - even_min
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.maxDifference("mmsmsym"))
