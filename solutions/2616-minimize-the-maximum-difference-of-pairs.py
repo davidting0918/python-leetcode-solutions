8bf9c7493d76161aea409c89911263acb15c795a
unknown <dting0918@gmail.com>
2025-06-21 08:32:51 +0800

    Add 2616 structure

---
+++ solutions/2616-minimize-the-maximum-difference-of-pairs.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/minimize-the-maximum-difference-of-pairs/?envType=daily-question&envId=2025-06-13
+
+from typing import List
+
+class Solution:
+    def minimizeMax(self, nums: List[int], p: int) -> int:
+        return 
+    
+
+
+if __name__ == "__main__":
+    s = Solution()
+    nums = [10,1,2,7,1,3]
+    p = 2
+    print(s.minimizeMax(nums, p))
