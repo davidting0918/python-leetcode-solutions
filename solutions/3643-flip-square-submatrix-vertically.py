6dd3045e073d5f087976f7b0d266c1db459f0466
unknown <dting0918@gmail.com>
2026-03-21 05:09:16 +0000

    Add 3643 solution

---
+++ solutions/3643-flip-square-submatrix-vertically.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def reverseSubmatrix(self, grid: List[List[int]], x: int, y: int, k: int) -> List[List[int]]:
+        
+
+        for col in range(y, y + k):
+            print(col)
+
+            start_row = x
+            for turn in range(k // 2):
+                print(start_row, start_row + k - turn - 1)
+                grid[start_row + turn][col], grid[start_row + k - turn - 1][col] = grid[start_row + k - turn - 1][col], grid[start_row + turn][col]           
+        return grid
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.reverseSubmatrix([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]], 1, 0, 3))
+    print(s.reverseSubmatrix([[14,3,18,16],[2,14,11,20],[19,19,4,15],[11,15,18,6]], 0, 0, 4))
